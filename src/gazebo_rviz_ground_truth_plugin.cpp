e4e78cabc32c5d073f9cfc0c519044919de48aea
ChristophTobler <toblech@student.ethz.ch>
2016-07-13 15:09:35 +0200

    add scale to arrow length and color alpha

--- src/gazebo_rviz_ground_truth_plugin.cpp
+++ src/gazebo_rviz_ground_truth_plugin.cpp
@@ -90,6 +90,8 @@ namespace gazebo
       double shaft_diameter = 0.05;
       double head_diameter = 0.1;
       double head_length = 0.2;
+      double color_alpha = 0.7;
+      double color_intensity = 1.0;
 
       tf::Quaternion q(qx, qy, qz, -qw);
       tf::Matrix3x3 m(q);
@@ -98,7 +100,7 @@ namespace gazebo
       tf::Vector3 m_off_row0 = m_offset.getRow(0);
       tf::Vector3 m_off_row1 = m_offset.getRow(1);
       tf::Vector3 m_off_row2 = m_offset.getRow(2);
-      
+
       double x_new = x*m_off_row0[0] + y*m_off_row0[1] + z*m_off_row0[2];
       double y_new = x*m_off_row1[0] + y*m_off_row1[1] + z*m_off_row1[2];
       double z_new = x*m_off_row2[0] + y*m_off_row2[1] + z*m_off_row2[2];
@@ -119,15 +121,15 @@ namespace gazebo
       x_marker.scale.x = shaft_diameter * scale;
       x_marker.scale.y = head_diameter * scale;
       x_marker.scale.z = head_length * scale;
-      x_marker.color.r = 1.0;
-      x_marker.color.a = 1.0;
+      x_marker.color.r = color_intensity;
+      x_marker.color.a = color_alpha;
       x_marker.points.resize(2);
       x_marker.points[0].x = x_new; //start
       x_marker.points[0].y = y_new;
       x_marker.points[0].z = z_new;
-      x_marker.points[1].x = x_new + x_axis[0]; //end
-      x_marker.points[1].y = y_new + x_axis[1];
-      x_marker.points[1].z = z_new + x_axis[2];
+      x_marker.points[1].x = x_new + x_axis[0] * scale; //end
+      x_marker.points[1].y = y_new + x_axis[1] * scale;
+      x_marker.points[1].z = z_new + x_axis[2] * scale;
 
       // Y direction arrow
       y_marker.header.frame_id = "world";
@@ -138,15 +140,15 @@ namespace gazebo
       y_marker.scale.x = shaft_diameter * scale;
       y_marker.scale.y = head_diameter * scale;
       y_marker.scale.z = head_length * scale;
-      y_marker.color.g = 1.0;
-      y_marker.color.a = 1.0;
+      y_marker.color.g = color_intensity;
+      y_marker.color.a = color_alpha;
       y_marker.points.resize(2);
       y_marker.points[0].x = x_new; //start
       y_marker.points[0].y = y_new;
       y_marker.points[0].z = z_new;
-      y_marker.points[1].x = x_new + y_axis[0]; //end
-      y_marker.points[1].y = y_new + y_axis[1];
-      y_marker.points[1].z = z_new + y_axis[2];
+      y_marker.points[1].x = x_new + y_axis[0] * scale; //end
+      y_marker.points[1].y = y_new + y_axis[1] * scale;
+      y_marker.points[1].z = z_new + y_axis[2] * scale;
 
       // Z direction arrow
       z_marker.header.frame_id = "world";
@@ -157,15 +159,15 @@ namespace gazebo
       z_marker.scale.x = shaft_diameter * scale;
       z_marker.scale.y = head_diameter * scale;
       z_marker.scale.z = head_length * scale;
-      z_marker.color.b = 1.0;
-      z_marker.color.a = 1.0;
+      z_marker.color.b = color_intensity;
+      z_marker.color.a = color_alpha;
       z_marker.points.resize(2);
       z_marker.points[0].x = x_new; //start
       z_marker.points[0].y = y_new;
       z_marker.points[0].z = z_new;
-      z_marker.points[1].x = x_new + z_axis[0]; //end
-      z_marker.points[1].y = y_new + z_axis[1];
-      z_marker.points[1].z = z_new + z_axis[2];
+      z_marker.points[1].x = x_new + z_axis[0] * scale; //end
+      z_marker.points[1].y = y_new + z_axis[1] * scale;
+      z_marker.points[1].z = z_new + z_axis[2] * scale;
 
       //path
       if (calc_path) {
